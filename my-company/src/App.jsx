18e5ddd3e5cb37b9afd32ec8dde7a41573417a1b
Talaku Getahun <getahuntalaku@gmail.com>
2024-08-18 23:53:25 +0300

    Fourt commit for my React component stayling project

---
+++ my-company/src/App.jsx
@@ -0,0 +1,14 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
+
+function App() {
+  return(
+    <Navbar />
+  );
+}
+export default App
---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,26 @@
+
+import { Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+function Navbar() {
+    return (
+        <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+  
+        <Route exact path="./Home" component={Home} />
+        <Route path="./About" component={About} />
+        <Route path="./Services" component={Services} />
+        <Route path="./Contact" component={Contact} />
+      </div>
+    );
+}
+
+export default Navbar
