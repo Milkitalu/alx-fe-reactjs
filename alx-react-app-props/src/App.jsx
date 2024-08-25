e6e8510ceea530e9795efbcf025922a3cbe4411f
Talaku Getahun <getahuntalaku@gmail.com>
2024-08-25 23:57:02 +0300

    commit made after correction

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.provider value= {userData}>
-      <ProfilePage userData={userData} />;
-    </UserContext.provider>
+    <div>
+      <UserContext.Provider value= {userData}>
+        <ProfilePage userData={userData} />;
+      </UserContext.Provider>
+    </div>
   );
       
 }
